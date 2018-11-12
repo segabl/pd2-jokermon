4ed22584f65a149b09deb81544d1149ae2a2c18b
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-11-12 13:36:22 +0100

    Save data on state change

---
+++ lua/gamestatemachine.lua
@@ -0,0 +1,8 @@
+local change_state_original = GameStateMachine.change_state
+function GameStateMachine:change_state(state, ...)
+  -- Save Jokermon on a state change from ingame to not ingame
+  if self:current_state_name():match("^ingame") and not state:name():match("^ingame") then
+    Jokermon:save(true)
+  end
+  return change_state_original(self, state, ...)
+end
